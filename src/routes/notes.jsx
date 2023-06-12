6ca2d68d2a86f1982a1b8dd33ef6eba5ea415f6a
Carl Lippert <carl.lippert@gmail.com>
2023-06-12 12:35:16 -0300

    editor is live but not functional

--- src/routes/notes.jsx
+++ src/routes/notes.jsx
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import LeftPanel from "../components/leftPanel";
 import FileExplorer from "../components/fileExplorer";
 
+import Editor from "@monaco-editor/react";
+
 export default function Notes() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
   return (
     <div className="flex flex-row h-full w-full ">
       <LeftPanel>
         <FileExplorer />
       </LeftPanel>
-      <div>
-        <Outlet />
+      <div className="flex flex-col w-full h-full bg-pink-100">
+        {/* <Outlet /> */}
+        {/* <MDEditor value={value} onChange={setValue} /> */}
+        <Editor
+          height="100vh"
+          theme="vs-dark"
+          defaultLanguage="markdown"
+          defaultValue="// some comment from carl"
+          className=" bg-green-300"
+        />
       </div>
     </div>
   );
